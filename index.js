3018ff9483ef080b865a1b712c72ebcf2ff1b333
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 03:21:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ function happyHolidays()
 {
   return `"Happy holidays"`;
 }
+
+function happyHolidaysTo(name)
+{
+  return `Happy holidays, ${name}`;
+}
+
+function happyHolidayTo(holiday,  name)
+{
+  return `Happy ${holiday} ${name}`;
+}
+
+
+}
