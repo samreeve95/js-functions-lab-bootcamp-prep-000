5da33964d60966d7d6eb28232481a46338ba8938
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 03:23:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,5 +23,9 @@ function happyHolidayTo(holiday,  name)
   return `Happy ${holiday}, ${name}!`;
 }
 
+function holidayCountdown(days, holiday)
+{
+  return `It's ${days} days until ${holiday}!`;
+}
 
 }
